bb6c91dd33d5d1ad8f8e315a6bfc763915ff0d16
Qi Zhu <821684824@qq.com>
2016-06-14 05:26:34 -0400

    17:26/14/6/2016

--- blog/sblog/urls.py
+++ blog/sblog/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import url
 urlpatterns = [
 	url(r'^bloglist/$','sblog.views.blog_list',name = 'bloglist'),
 	url(r'^blog/(?P<id>\d+)/$','sblog.views.blog_show',name = 'detailblog'),
+	
+
 ]
